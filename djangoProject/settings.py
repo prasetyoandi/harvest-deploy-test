d53e7f16bbd1e75f390b95135ac7814bf625c397
prasetyoandi <smokelesscloud7@gmail.com>
2024-04-26 16:12:07 +0700

    db change on settings.py again

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -93,9 +93,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': 'WAaVVzepcTghZmrZFQlpnfDWNVcyPoZl',
+        'PASSWORD': 'icVmEACcjTwiADJcGQbgJeqNPaqmyqkI',
         'HOST': 'monorail.proxy.rlwy.net',
-        'PORT': '20830',
+        'PORT': '26541',
 
     }
 }
