cce0741e358cfb3723510225a489be0d950e0304
prasetyoandi <smokelesscloud7@gmail.com>
2024-04-29 00:03:47 +0700

    change config

--- build.sh
+++ build.sh
@@ -6,10 +6,10 @@
 echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
 
-echo "Make migrations... "
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py makemigrations calculate --noinput
-python3.9 manage.py migrate --noinput
+# echo "Make migrations... "
+# python3.9 manage.py makemigrations --noinput
+# python3.9 manage.py makemigrations calculate --noinput
+# python3.9 manage.py migrate --noinput
 
 echo "Collect stetic.."
 python3.9 manage.py collectstatic --noinput --clear
