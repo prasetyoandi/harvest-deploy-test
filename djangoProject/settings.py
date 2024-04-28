422d10d51c89c4ec444aca5e5ba1696dcca6fc02
prasetyoandi <smokelesscloud7@gmail.com>
2024-04-29 01:26:13 +0700

    change config

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 from os.path import join
-import dj_database_url
-from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
