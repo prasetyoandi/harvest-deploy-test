6d6f82add2debcfbfd6d998f2c90ce7ad4f8cbd6
prasetyoandi <smokelesscloud7@gmail.com>
2024-04-29 00:58:33 +0700

    change config

--- build_files.sh
+++ build_files.sh
@@ -4,7 +4,7 @@
 
 # Build the project
 echo "Building the project..."
-python3.9 -m pip install -r requirements.txt
+pip install -r requirements.txt
 
 echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput
