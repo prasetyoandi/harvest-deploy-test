f19d11e6ada5ac73e2ad767d2f0f21bd80186685
prasetyoandi <smokelesscloud7@gmail.com>
2024-04-29 00:48:39 +0700

    change config

--- build_files.sh
+++ build_files.sh
@@ -1,15 +1,14 @@
-# !/bin/bash
+#!/bin/bash
 
 # python3.9 -m virtualenv venv
 
-# build the project
+# Build the project
 echo "Building the project..."
 python3.9 -m pip install -r requirements.txt
 
-# echo "Make migrations... "
-# python3.9 manage.py makemigrations --noinput
-# python3.9 manage.py makemigrations calculate --noinput
-# python3.9 manage.py migrate --noinput
+echo "Make Migration..."
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
 
-echo "Collect stetic.."
+echo "Collect Static..."
 python3.9 manage.py collectstatic --noinput --clear
