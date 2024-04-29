0aa50e70bc55d5ee5eff4dfd6929f307a267a586
prasetyoandi <smokelesscloud7@gmail.com>
2024-04-29 10:43:49 +0700

    added pip in build

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 
 set -o errexit
 
-python -m pip install -r requirements.txt
+python3.9 -m pip install -r requirements.txt
 
 python manage.py makemigrations --noinput
 python manage.py makemigrations calculate --noinput
