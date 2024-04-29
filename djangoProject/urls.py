db85e9687c0bedd9c2c15b42607098c22181d428
prasetyoandi <smokelesscloud7@gmail.com>
2024-04-29 12:23:44 +0700

    build change

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from calculate.views import Calculate
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('users.urls')),
@@ -27,6 +29,10 @@ urlpatterns = [
     path('calculate', Calculate.as_view(), name='calculate')
 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 
 # handler404 = 'djangoProject.errorViews.handler404'
 # handler500 = 'djangoProject.errorViews.handler500'
