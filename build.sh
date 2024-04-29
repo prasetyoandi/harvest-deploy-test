53f1f9c19726586374732ac6f9b8e31381237eb4
prasetyoandi <smokelesscloud7@gmail.com>
2024-04-29 11:11:39 +0700

    added pip in build

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # python3.9 -m virtualenv venv
 
-python3.9 -m pip install -r requirements.txt
+python3.9 -m pip install -r /vercel/path0/requirements.txt
 
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py makemigrations calculate --noinput
